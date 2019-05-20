17899d864753683c7ac12bae44dab3d37cf77f7a
thanawat <tha_chookes@hotmail.com>
2019-05-20 22:44:33 +0700

    fix route to lowercase

--- src/core/routes.js
+++ src/core/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Main from '../containers/Main'
+import Main from '../containers/main'
 
 const Routes = () => (
   <Router>
